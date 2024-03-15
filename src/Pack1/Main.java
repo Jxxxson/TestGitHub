030b87deb13c6bc1538a46a55ba2bbbcb2c764bc
Jason HIOE <jasonhioe1@gmail.com>
2024-03-14 14:37:53 -1000

    Ceci est la version 5

--- src/Pack1/Main.java
+++ src/Pack1/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Bonjour, version 1.");
 		System.out.println("Bonjour, version 2 prout.");
+		System.out.println("prout prout prout prout prout.CLPplVersion.");
 
 	}
 
